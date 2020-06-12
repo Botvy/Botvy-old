dd3994f7137a824c78f5aecd64d0ebbe223a43a3
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-12 23:02:29 +0200

    feat(ioc): added the service constants
    The service constants contains symbols for various services (like logging, the plugin system).

---
+++ packages/framework/src/ioc/ServiceConstants.ts
@@ -0,0 +1,22 @@
+/**
+ * Contains the constants for the dependency injection container
+ */
+export const ServiceConstants = {
+    System: {
+        Logger: Symbol.for('System.Logger'),
+        LoggerOptions: {
+            Transports: Symbol.for('System.LoggerOptions.Transports'),
+        },
+        Plugin: {
+            InitializationSide: Symbol.for('System.Plugin.InitializationSide'),
+            Loader: {
+                PluginLoader: Symbol.for('System.Plugin.Loader.PluginLoader'),
+                DirectoryLoader: {
+                    directoryPath: Symbol.for(
+                        'System.Plugin.Loader.DirectoryLoader.DirectoryPath',
+                    ),
+                },
+            },
+        },
+    },
+};
