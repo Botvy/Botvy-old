61cc5cfaba64bb377ab170e4f6fb1dcc8cf179e1
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-14 00:57:11 +0200

    feat(logger): added the configureLogger method
    The configureLogger method configures the given logger and sets the given name as tag. The tag will
    be used by the logger to show from where the logged messages are coming from.

--- packages/framework/src/logging/logger.ts
+++ packages/framework/src/logging/logger.ts
@@ -1,19 +1,35 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, Logger, transports } from 'winston';
 
 const ConsoleTransport = new transports.Console();
 
 export const logger = createLogger({
     level: process.env.LOG_LEVEL ?? 'info',
-    transports: [
-        ConsoleTransport,
-    ],
+    transports: [ConsoleTransport],
     format: format.combine(
         format.colorize(),
         format.timestamp({
             format: 'DD.MM.YYYY HH:mm:ss',
         }),
-        format.printf(({ timestamp, level, message, ...rest }) => {
+        format.printf(({ timestamp, level, message }) => {
             return `[${timestamp}] [${level}]: ${message}`;
         }),
     ),
 });
+
+/**
+ * Configures the given logger and sets the name as tag.
+ * The tag will be used by the logger to show from which
+ * class the messages are coming from.
+ *
+ * @param logger The logger which should be configured
+ * @param name The name of the class which uses the logger
+ */
+export const configureLogger = (logger: Logger, name: string) => {
+    logger.configure({
+        defaultMeta: {
+            tags: [name],
+        },
+        level: logger.level,
+        transports: logger.transports,
+    });
+};
