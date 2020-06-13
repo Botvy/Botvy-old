f3734ae5b7e7c903120b65fff33a3d183f63e391
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-13 02:59:53 +0200

    refactor(schema): updated the pluginDescriptor schema
    The schema now also checks, if the paths are in the plugin directory

--- packages/framework/src/schema/pluginDescriptor.ts
+++ packages/framework/src/schema/pluginDescriptor.ts
@@ -1,5 +1,29 @@
 import joi from '@hapi/joi';
 
+/**
+ * Checks if the path starts with ".."
+ *
+ * @param path The path which should be checked
+ */
+export const isInPluginDirectory = (path: string) =>
+    path.includes('..') === false;
+
+/**
+ * Validates the given value to be inside the plugin directory
+ *
+ * @param value The value which should be validated
+ */
+const pathValidatorFunction = (value: string): string => {
+    if (!isInPluginDirectory(value)) {
+        throw new Error('The entrypoint must be in the plugin directory');
+    }
+
+    return value;
+};
+
+/**
+ * Defines the schema for the IPluginDescriptionFile
+ */
 export const pluginDescriptorSchema = joi.object({
     id: joi.string().required(),
     name: joi.string().required(),
@@ -15,7 +39,27 @@ export const pluginDescriptorSchema = joi.object({
         }),
     ),
     dependsOn: joi.array().items(joi.string().required()),
-    entrypoint: joi.string().required(),
-    additionalContainerBindings: joi.array().items(joi.string().required()),
-    sectionComponents: joi.object(),
+    entrypoint: joi.string().custom(pathValidatorFunction).required(),
+    additionalContainerBindings: joi
+        .array()
+        .items(joi.string().custom(pathValidatorFunction).required()),
+    sectionComponents: joi
+        .object()
+        .custom((value: Record<string, string[]>) => {
+            for (const key in Object.keys(value)) {
+                const objectValue = value[key];
+
+                for (const entry in objectValue) {
+                    if (isInPluginDirectory(entry)) {
+                        continue;
+                    }
+
+                    throw new Error(
+                        'The section components must be inside the plugin directory',
+                    );
+                }
+            }
+
+            return value;
+        }),
 });
