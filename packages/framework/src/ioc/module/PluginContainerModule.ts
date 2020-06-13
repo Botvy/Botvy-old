c1d09a730331a7f4c3a52961887738fa4bc79743
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-13 02:42:20 +0200

    refactor(plugin): the plugins directory should be based on the current working directory

--- packages/framework/src/ioc/module/PluginContainerModule.ts
+++ packages/framework/src/ioc/module/PluginContainerModule.ts
@@ -1,8 +1,10 @@
 import { ContainerModule } from 'inversify';
-import { PluginManager } from '../../plugin/PluginManager';
-import { PluginDependencyResolver } from '../../plugin/PluginDependencyResolver';
-import { DirectoryPluginLoader } from '../../plugin/loader/DirectoryPluginLoader';
+import { cwd } from 'process';
+
 import { ServiceConstants } from '../../ioc/ServiceConstants';
+import { DirectoryPluginLoader } from '../../plugin/loader/DirectoryPluginLoader';
+import { PluginDependencyResolver } from '../../plugin/PluginDependencyResolver';
+import { PluginManager } from '../../plugin/PluginManager';
 
 export class PluginContainerModule extends ContainerModule {
     constructor() {
@@ -15,7 +17,7 @@ export class PluginContainerModule extends ContainerModule {
             bind(
                 ServiceConstants.System.Plugin.Loader.DirectoryLoader
                     .directoryPath,
-            ).toConstantValue('./plugins');
+            ).toConstantValue(`${cwd()}/plugins`);
         });
     }
 }
