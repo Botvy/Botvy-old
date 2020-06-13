b4205471c62e901c30595a9b5f49ac8ff2074c8d
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-14 01:08:44 +0200

    feat(ioc): added the client container
    The client container binds additional client side IPC event handlers to the container before it is
    returned

---
+++ packages/client/src/ioc/container.ts
@@ -0,0 +1,16 @@
+import { getConfiguredContainer } from '@botvy/framework/dist/ioc/container';
+import { IPCEventHandler } from '@botvy/framework/dist/ipc/IPCEventHandler';
+import { InitializationSide } from '@botvy/framework/dist/plugin/InitializationSide';
+
+import { LoadClientSettingsIPCHandler } from '../ipc/LoadClientSettingsIPCHandler';
+
+export const getClientContainer = async () => {
+    const container = await getConfiguredContainer(InitializationSide.CLIENT);
+
+    container
+        .bind(IPCEventHandler)
+        .to(LoadClientSettingsIPCHandler)
+        .inSingletonScope();
+
+    return container;
+};
