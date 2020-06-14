b17a2032377562b407e73d7c40e2f25531055492
Yannick Fricke <yannickfricke@googlemail.com>
2020-06-14 08:24:42 +0200

    feat(ipc): added the LoadThemeIPCHandler
    The LoadThemeIPCHandler loads a theme with the ThemeManager and returns the result back to the
    client

--- packages/client/src/ioc/container.ts
+++ packages/client/src/ioc/container.ts
@@ -1,16 +1,19 @@
 import { getConfiguredContainer } from '@botvy/framework/dist/ioc/container';
 import { IPCEventHandler } from '@botvy/framework/dist/ipc/IPCEventHandler';
 import { InitializationSide } from '@botvy/framework/dist/plugin/InitializationSide';
+import { Container } from 'inversify';
 
 import { LoadClientSettingsIPCHandler } from '../ipc/settings/LoadClientSettingsIPCHandler';
+import { LoadThemeIPCHandler } from '../ipc/theming/LoadThemeIPCHandler';
 
-export const getClientContainer = async () => {
+export const getClientContainer = async (): Promise<Container> => {
     const container = await getConfiguredContainer(InitializationSide.CLIENT);
 
     container
         .bind(IPCEventHandler)
         .to(LoadClientSettingsIPCHandler)
         .inSingletonScope();
+    container.bind(IPCEventHandler).to(LoadThemeIPCHandler).inSingletonScope();
 
     return container;
 };
